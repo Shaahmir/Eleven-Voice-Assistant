4987a95bf28c60987f876d61a7c69181f33a19b4
Muhammad Shahmir <159647518+Shaahmir@users.noreply.github.com>
2026-01-03 12:35:56 +0500

    Update Eleven.py

--- Eleven.py
+++ Eleven.py
@@ -1,14 +1,15 @@
-import tkinter as tk #importing Tkinter for GUI
-from tkinter.scrolledtext import ScrolledText #importig some part of Tkinter.scrolled Text
-import pyttsx3 #Stands for Python Text-to-speech for voice output
-import speech_recognition as sr #for voice recognition
-from PIL import ImageGrab
+import pyttsx3
+import speech_recognition as sr
+import pyautogui
+import pywhatkit
+import tkinter as tk
+from tkinter.scrolledtext import ScrolledText
+import webbrowser
+from datetime import datetime 
+import os 
+import threading
 
-#Default Python Libraries
-import webbrowser #for opening URLs
-from datetime import datetime #for getting today's Date and Time
-import os #importing os just to open calculator
-import threading #for multi tasking
+# -------------------- App Dictionary --------------------
 
 apps = {
     "notepad": "notepad",
@@ -35,7 +36,6 @@ apps = {
 
     "player": "wmplayer",
     "spotify": "spotify",
-
     "paint": "mspaint",
     "snipping tool": "snippingtool",
     "camera": "start microsoft.windows.camera:",
@@ -45,158 +45,208 @@ apps = {
     "python idle": "idle",
 }
 
-# ----------------- Initialize TTS -----------------
-engine = pyttsx3.init() #initializing voice agent
-voices = engine.getProperty('voices') #getting all available voice agents
-engine.setProperty('voice', voices[1].id)  # Female voice 0 for Male
-engine.setProperty('rate', 160) #160 wpm
-engine.setProperty('volume', 1) #with full volume
+# -------------------- Output Text --------------------
 
-# ----------------- Helper Function to Add Text -----------------
 def add_text(text):
-    output_text.configure(state='normal') #enable editing mode
-    output_text.insert(tk.END, text + "\n") #inserting text
-    output_text.configure(state='disabled') #disable editing mode
-    output_text.see(tk.END) #automatically scrolls down to show the latest message
+    ouput_text.configure(state="normal")
+    ouput_text.insert(tk.END, text + "\n")
+    ouput_text.configure(state="disabled")
+    ouput_text.see(tk.END)
 
-# ----------------- Open Web -----------------
+# -------------------- Initialize Voice Model --------------------
+
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty('voice',voices[1].id)
+engine.setProperty('rate',160)
+engine.setProperty('volume',1)
+
+# -------------------- Website Opener --------------------
+
 def web_opener(message):
-    message_list = message.split(' ')
-    idx = message_list.index("open")
-    des_msg = message_list[idx + 1 ]
-    add_text(f"El: Opening {des_msg} in Chrome")
-    engine.say(f"Opening {des_msg} in Web")
-    webbrowser.open(f"https://{des_msg}.com")
+    list_1=list(message.split())
+    i=list_1.index("open")
+    desire_variable=list_1[i+1]
+    add_text(f"El: opening, {desire_variable} in chrome")
+    engine.say(f"opening {desire_variable} in chrome")
+    webbrowser.open(f"http://www.{desire_variable}.com")
 
-#----------------- Open App -----------------
+# -------------------- App Opener --------------------
 
 def app_opener(message):
-    message_list = message.split(' ')
-    idx = message_list.index('open')
-    des_msg_list = message_list[idx + 1:]
-    des_msg = ''
-    for x in des_msg_list:
-        des_msg += (x + ' ')
-    if des_msg.strip() in apps.keys():
-        add_text(f"El: Opening {des_msg.strip()}")
-        engine.say(f"Opening {des_msg.strip()}")
-        os.system(apps[des_msg.strip()])
+    list_1=list(message.split())
+    i=list_1.index("open")
+    desire_variable=list_1[i+1: ]
+    str_1=""
+    for x in desire_variable:
+        str_1 += (x+" ")
+    str_1 = str_1.strip()
+    if str_1 in apps:
+        os.system(apps[str_1])
+        add_text(f"El: opening, {desire_variable}")
+        engine.say(f"opening {desire_variable}")
     else:
-        add_text(f"El: Sorry, I didn't find {des_msg.strip()} anywhere")
-        engine.say(f"Sorry, I didn't find {des_msg.strip()} anywhere")
+        add_text(f"El: sorry, I didn't find {desire_variable} anywhere")
+        engine.say(f"sorry, I didn't find {desire_variable} anywhere")      
 
+# -------------------- Action Function --------------------
 
-# ----------------- Command Processing -----------------
-def process_command(message):
-    add_text(f"You said: {message}")
+def action(message):
+    add_text(f"you said: {message}")
 
+    # ----------------- Opening Google and searching -----------------
     if "open google and search" in message:
-        add_text(f"El: Opening Google and searching {message}")
-        engine.say(f"Opening Google and searching {message}")
-        webbrowser.open(f"https://www.google.com/search?q={message.replace(' ', '+')}")
+        list_1 = list(message.split())
+        i = list_1.index("search")
+        desire_variable = list_1[i + 1:]
+        str_1 = ""
+        for x in desire_variable:
+            str_1 += (x + " ")
+        str_1 = str_1.strip()
+        add_text(f"El: Opening Google and searching for {str_1}")
+        engine.say(f"Opening Google and searching for {str_1}")
+        webbrowser.open(f"https://www.google.com/search?q={str_1}")
 
+    # ----------------- Opening Youtube and searching -----------------          
     elif "open youtube and search" in message:
-        add_text(f"El: Opening YouTube and searching {message}")
-        engine.say(f"Opening YouTube and searching {message}")
-        webbrowser.open(f"https://www.youtube.com/results?search_query={message.replace(' ', '+')}")
+        list_1 = list(message.split())
+        i = list_1.index("search")
+        desire_variable = list_1[i + 1:]
+        str_1 = ""
+        for x in desire_variable:
+            str_1 += (x + " ")
+        str_1 = str_1.strip()
+        add_text(f"El: Opening Youtube and searching for {str_1}")
+        engine.say(f"Opening Youtube and searching for {str_1}")
+        webbrowser.open(f"https://www.youtube.com/results?search_query={str_1}")
 
+    # ----------------- Playing the first video appears on yt after searching -----------------
+    elif "play" in message:
+        list_1 = list(message.split())
+        i = list_1.index("play")
+        desire_variable = list_1[i+1:]
+        str_1 = " "
+        for x in desire_variable:
+            str_1 += (x + " ")
+        str_1 = str_1.strip()
+        add_text(f"El: Currently Playing {str_1}")
+        engine.say(f"Roger that, playing {str_1} on youtube")
+        pyautogui.press("volumeup", presses=100)
+        pywhatkit.playonyt(str_1)
+
+    # ----------------- Mute -----------------
+    elif "mute" in message:
+        add_text(f"El: system muted {message}")
+        pyautogui.press("volumemute")
+
+    # ----------------- Umute -----------------
+    elif "unmute" in message:
+        add_text(f"El: system muted {message}")
+        pyautogui.press("volumemute")
+        engine.say(f"Umuted the system {message}")
+
+    # ----------------- Opening direct website link -----------------    
     elif "open" in message and ("web" in message or "in chrome" in message):
         web_opener(message)
 
+    # ----------------- Opening apps -----------------
     elif "open" in message:
         app_opener(message)
 
-    elif "screenshot" in message:
-        add_text("El: Screenshot Taken")
-        engine.say("Screenshot Taken")
-        screenshot = ImageGrab.grab()
-        screenshot.save(f"screenshot{datetime.now().strftime('%f')}.png")
-
+    # ----------------- Showing Time -----------------
     elif "time" in message:
-        time_now = datetime.now().strftime("%I:%M %p") #Hours Minutes AM/PM
+        time_now = datetime.now().strftime("%I:%M %p")
         add_text(f"El: The current time is {time_now}")
         engine.say(f"The current time is {time_now}")
 
+    # ----------------- Showing Date -----------------
     elif "date" in message:
-        date_now = datetime.now().strftime("%B %d %Y") #Month Date Year
-        add_text(f"El: The current time is {date_now}")
-        engine.say(f"Today's date is {date_now}")
+        today = datetime.now().strftime("%d %B %Y")
+        add_text(f"El: The current time is {today}")
+        engine.say(f"The current time is {today}")
+    
+    # ----------------- Screenshot -----------------
+    elif "screenshot" in message:
+        add_text("El: Screenshot Taken")
+        engine.say("Screenshot Taken")
+        screenshot = pyautogui.screenshot()
+        screenshot.save(f"Screenshot {datetime.now().strftime('%f')}.png")
 
-    elif "hello" in message or "hey" in message or "hi" in message or "yo" in message or "heya" in message:
+    #----------------- Greetings in English -----------------
+    elif ("hello" in message or "hey" in message or "hi" in message) and not("what" in message):
         add_text(f"El: Hello, How may I help you?")
         engine.say("Hello, How may I help you?")
 
-    elif "assalam" in message or "salam" in message:
-        add_text(f"El: Wallikum Assalam, How may I help you?")
+    #----------------- Greetings in Urdu -----------------
+    elif "salam" in message or "assalam" in message:
+        add_text(f"El: Wallikum Assalam, How may I help you?") 
         engine.say("Wallikum Assalam, How may I help you?")
 
+    #----------------- If none of above match search it on google -----------------
     else:
         add_text(f"El: searching {message} on Google")
         engine.say(f"searching {message} on Google")
-        webbrowser.open(f"https://www.google.com/search?q={message.replace(' ', '+')}")
+        webbrowser.open(f"https://www.google.com/search?q={message}")
 
 # ----------------- Voice Recognition -----------------
-def take_command():
+
+def recognizer():
     recognizer = sr.Recognizer()
 
     try:
-        with sr.Microphone(device_index=2) as source: #automatically open/close the microphone and set souce as Microphone, 2 = Conexant ISST Audio
-            add_text("Calibrating microphone...") #add this text
-            recognizer.adjust_for_ambient_noise(source, duration=1) #set energy_threshold and ignore bg noise
-            add_text("Listening...")#add this text
+        with sr.Microphone(device_index=2) as source:
+            add_text("Calibrating Microphone...")
+            recognizer.adjust_for_ambient_noise(source,duration=1)
+            add_text("Listening...")
+            audio = recognizer.listen(source,timeout=10,phrase_time_limit=10)
+            message= recognizer.recognize_google(audio)
+            action(message.lower())
 
-            audio = recognizer.listen( #starts listening
-                source, #The microphone object you got from with sr.Microphone(...) as source:
-                timeout=10, #Maximum seconds to wait for someone to start speaking. If no speech is detected in 10 seconds, it raises WaitTimeoutError.
-                phrase_time_limit=10 #Maximum length of speech to record, in seconds. Even if you keep talking, it stops recording after 10 seconds.
-            )
-
-        add_text("Recognizing...") #add this text
-
-        message = recognizer.recognize_google(audio) #sends the recorded audio to Google’s speech recognition engine
-        process_command(message.lower()) #Calling process_command fucntion
-
-    except sr.WaitTimeoutError: #After 10 sec
+    except sr.WaitTimeoutError:
         add_text("Eleven: Listening timed out, please try again") #add this text
-    except sr.UnknownValueError: #if the system not comprehending
-        add_text("Eleven: Could not understand, please speak clearly") #add this text
-    except sr.RequestError: #if microphone not working
-        add_text("Server: Speech service unavailable") #add this text
-    except Exception as e: #catch every error and display it as a string
-        add_text(f"Error: {str(e)}") #add this text
 
-# ----------------- Run in Thread -----------------
+    except sr.UnknownValueError: ##add text 
+        add_text("Eleven: Could not understand, please speak clearly") #add this text
+
+    except sr.RequestError: ##add text
+        add_text("Server: Speech service unavailable") #add this text
+
+    except Exception as e:
+         add_text(f"Error:{str(e)}")
+
+# ----------------- Threading -----------------
+ 
 def start_listening():
-    threading.Thread(target=take_command).start() #call take_command function and starts the thread running in the background
+    threading.Thread(target=recognizer).start()
 
 # ----------------- GUI -----------------
-root = tk.Tk() #main GUI Window
-root.title("Eleven - Voice Assistant ") #window name
-root.iconbitmap("./images/favicon_transparent.ico")  #favicon
-root.geometry("640x480") #window size
-root.resizable(False, False) #make it unable to resize
-root.configure(bg="#E6E6FA") #Adds a background in the window
 
-# ----------------- Main Heading -----------------
+root = tk.Tk()
+root.title("Eleven - Voice Assistant")
+root.iconbitmap("./images/favicon_transparent.ico")
+root.geometry("1080x700")
+root.configure(bg="#E6E6FA")
+
+# ----------------- Heading -----------------
+
 icon_img = tk.PhotoImage(file="./images/logo.png")
-title_label = tk.Label(root, text="Eleven - A Voice Assistant",
-                       font=("Segoe UI", 24, "bold"), fg="#6A5ACD", bg="#E6E6FA", image=icon_img, compound="left",  padx=10)
-title_label.pack(pady=20)
+label_text = tk.Label(root, text="Eleven - A Voice Assistant", font=("Segoe UI", 24, "bold"), fg="#6A5ACD", bg="#E6E6FA", image=icon_img, compound="left", padx=10)
+label_text.pack(pady=20)
 
-# ----------------- Main Heading -----------------
-output_text = ScrolledText(root, width=60, height=12, font=("Segoe UI", 12), wrap=tk.WORD,
-                           bg="#FFFFFF", fg="#606060", state='disabled')
-output_text.pack(pady=20)
+# ----------------- Output textarea -----------------
 
-# ----------------- Speak Button -----------------
-listen_button = tk.Button(root, text="Speak", font=("Segoe UI", 14), command=start_listening, 
-                          bg="#9370DB", fg="white", width=10)
+ouput_text = ScrolledText(root, width=60, height=12, font=("Segoe UI", 12), wrap=tk.WORD, bg="#FFFFFF", fg="#606060", state="disabled")
+ouput_text.pack(pady=20)
+
+# ----------------- Speak button -----------------
+
+listen_button = tk.Button(root, text="Speak", width=10, font=("Segoe UI", 14), bg="#9370DB", fg="#FFFFFF", command=start_listening)
 listen_button.pack(pady=20)
 
-# ----------------- Greetings-----------------
-add_text("Eleven: How May I Help You? Press Speak To Start.")
+# ----------------- Starting text -----------------
+
+add_text("El: Welcome to Eleven a Voice Assistant Chatbot")
 engine.runAndWait()
 engine.say("Welcome to Eleven A Voice Assistant Chatbot")
 
-#----------------- Calling in a loop -----------------
 root.mainloop()
